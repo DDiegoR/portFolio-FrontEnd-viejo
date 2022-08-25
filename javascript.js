8d16b964825b1c9787194cf64ae467fa0915ed53
DDiegoR <91162803+DDiegoR@users.noreply.github.com>
2022-08-25 00:37:54 -0300

    seccion 2 v.2

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+
+// EVENTOS BOTONES DE SECCION 2
+function cambiaF(){
+    document.getElementById("facultad").style.display = "block";
+    document.getElementById("cursoDW").style.display = "none";
+    document.getElementById("argentinaPrograma").style.display = "none";
+}
+
+function cambiaDW(){
+    document.getElementById("facultad").style.display = "none";
+    document.getElementById("cursoDW").style.display = "block";
+    document.getElementById("argentinaPrograma").style.display = "none";
+}
+
+function cambiaAP(){
+    document.getElementById("facultad").style.display = "none";
+    document.getElementById("cursoDW").style.display = "none";
+    document.getElementById("argentinaPrograma").style.display = "block";
+}
