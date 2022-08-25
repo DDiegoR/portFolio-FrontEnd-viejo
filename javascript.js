c825daab10e5645b7ebdfe964b1863f1e07171cb
DDiegoR <91162803+DDiegoR@users.noreply.github.com>
2022-08-25 18:35:48 -0300

    seccion 2 v.2.2

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 
 
-// EVENTOS BOTONES DE SECCION 2
+
+// EVENTOS BOTONES ESTUDIOS SECCION 2
 function cambiaF(){
     document.getElementById("facultad").style.display = "block";
     document.getElementById("cursoDW").style.display = "none";
@@ -17,4 +18,19 @@ function cambiaAP(){
     document.getElementById("facultad").style.display = "none";
     document.getElementById("cursoDW").style.display = "none";
     document.getElementById("argentinaPrograma").style.display = "block";
+}
+
+
+// EVENTO IMAGEN DISEÑOWEB
+function cambiarTamanioImagen(){
+    var identificador = document.getElementById("ien-titulo");
+    if (identificador.style.width == "10%") {
+        identificador.style.height = "400px";
+        identificador.style.width = "500px";
+    }
+    else {
+        identificador.style.height = "10%";
+        identificador.style.width = "10%";
+    }
+    img.style.transition = "transform 0.25s ease";
 }
