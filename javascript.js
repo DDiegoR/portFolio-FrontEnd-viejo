8b48cf6a4adfa64965c07311634420a62ddb1b90
DDiegoR <91162803+DDiegoR@users.noreply.github.com>
2022-09-24 02:33:11 -0300

    esquema v.2

--- javascript.js
+++ javascript.js
@@ -75,3 +75,23 @@ function cambiarTamanioImagen(){
 }
 
 document.getElementById("ien-titulo").addEventListener("click", cambiarTamanioImagen);
+
+
+// SECCION PROYECTOS
+
+// CAJA IMAGENES 
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
